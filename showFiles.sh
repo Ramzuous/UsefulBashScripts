ff4655935f6902ff53f1d53f21b437bce7ba562f
PatrykDróżdż <patrol687@wp.pl>
2017-05-24 13:25:32 +0200

    message

--- showFiles.sh
+++ showFiles.sh
@@ -2,18 +2,21 @@
 
 shFiles(){
 
-	pathFile=$1
+	pathFile="/mnt/c/Users/Patryk/Desktop"
 
 	if [$pathFile -n]; #sprawdzanie czy zmienna nie jest pusta
 	then 
 		echo "nie podano ścieżki"
 	else 
 		
+		echo "1. $pathFile"
+		
 		echo -n "liczba plików w: $pathFile to: " # -n brak przejścia do nowej lini
 		find  $pathFile -maxdepth 1 | wc -l 
 		ls --color=auto -l #koloruje i pokazuje date modyfikacji, sortuje
 		
+		
 	fi
 }
 
-shFiles "/mnt/c/Users/Patryk/Desktop"
+shFiles
