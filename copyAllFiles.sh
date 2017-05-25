c78762dce090486da93073a9a4107f3815697fae
PatrykDróżdż <patrol687@wp.pl>
2017-05-25 16:36:39 +0200

    Some progress

--- copyAllFiles.sh
+++ copyAllFiles.sh
@@ -4,14 +4,20 @@ cpyFiles(){
 	
 	source=$1
 	dest=$2
+
+	filesAll=`find $source -maxdepth 1`
 	
-	loop=`find $source -maxdepth 1 | wc -l `-1
-	
-	for (( i=0; $i < $loop; i++)); do
-	    files[$i] = $i
-		
-		echo $(files[$i])
+	files=$(echo $filesAll | tr "/" "\n")
+	a=1
+	for fileTab in $files
+	do
+		echo ">$filesTab"
+		a=$[a+1]
 	done
+	
+	echo "$filesAll"
+	echo "$a"
 }
 
-cpyFiles "/mnt/c/Users/Patryk/Documents/CV"
+cpyFiles "/mnt/c/Users/Patryk/Desktop/test/" #Documents/CV
+
