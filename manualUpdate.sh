0f1a26119ac0f3597f72aca91c336cc0e1f601e0
PatrykDróżdż <patrol687@wp.pl>
2017-05-26 17:45:53 +0200

    some changes added

---
+++ manualUpdate.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+cpyFiles(){
+	
+	source=$1
+	dest=$2
+
+	filesAll=`find $source -maxdepth 1 -mindepth 1 -type f`
+	echo "$filesAll"
+	#cp -r $filesAll $dest
+
+}
+
+checkDirs(){
+	
+	source=$1
+	
+	filesAll=`find $source -maxdepth 1 -mindepth 1 -type d`
+	
+	echo "$filesAll"
+}
+
+checkDirs "/mnt/c/Users/Patryk/Desktop/test/"
+cpyFiles "/mnt/c/Users/Patryk/Desktop/test/"
