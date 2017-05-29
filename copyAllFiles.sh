7cc6136cc0362c825bdfb01ac803f3b3fcc20806
PatrykDróżdż <patrol687@wp.pl>
2017-05-29 09:34:20 +0200

    Update works

--- copyAllFiles.sh
+++ copyAllFiles.sh
@@ -5,7 +5,7 @@ cpyFiles(){
 	source=$1
 	dest=$2
 
-	filesAll=`find $source -mindepth 1`
+	filesAll=`find $source -maxdepth 1 -mindepth 1 -type f`
 	
 	cp -r $filesAll $dest
 
---
+++ update.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+upgrade(){
+	
+	source=$1
+	dest=$2
+
+	for file in $source/*; do 
+		cp -r $file $dest 
+		
+	done;
+}
+
+upgrade "/mnt/c/Users/Patryk/Desktop/test" "/mnt/c/Users/Patryk/Desktop/result2"
