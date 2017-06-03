84e6ffdf2b3c65a248bbfd04611c3d0131e5cbda
PatrykDrozdz <patrol687@wp.pl>
2017-06-03 11:03:01 +0200

    thinks needed to be clear

--- copyAllFiles.sh
+++ copyAllFiles.sh
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-#!/bin/bash
-
-cpyFiles(){
-	
-	source=$1
-	dest=$2
-
-	filesAll=`find $source -maxdepth 1 -mindepth 1 -type f`
-	
-	cp -r $filesAll $dest
-
-	echo "Wszystkie pliki z $source skopiowano do $dest"
-	
-}
-
-cpyFiles "/mnt/c/Users/Patryk/Desktop/test/" "/mnt/c/Users/Patryk/Desktop/result/"
-=======
 #!/bin/bash
 
 cpyFiles(){
@@ -32,4 +14,4 @@ cpyFiles(){
 }
 
 cpyFiles $1 $2
->>>>>>> c2923e214a06bc25eebc951ef5d857b8bd9569fa
+
--- update.sh
+++ update.sh
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-#!/bin/bash
-
-upgrade(){
-	
-	source=$1
-	dest=$2
-
-	for file in $source/*; do 
-		cp -r $file $dest 
-		
-	done;
-}
-
-upgrade "/mnt/c/Users/Patryk/Desktop/test" "/mnt/c/Users/Patryk/Desktop/result2"
-
-echo "Aktualizacja została wykonana..."
-=======
 #!/bin/bash
 
 upgrade(){
@@ -32,4 +14,4 @@ upgrade(){
 }
 
 upgrade $1 $2
->>>>>>> c2923e214a06bc25eebc951ef5d857b8bd9569fa
+
